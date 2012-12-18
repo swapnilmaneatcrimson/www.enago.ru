aa8e36f42e55594222a2306c1f21718903f6a205
MKTG_ADMIN <MKTG_ADMIN@4568d011-afc3-487c-8030-59b888a8bcde>
2012-12-18 14:13:28 +0000

    first version
    git-svn-id: http://192.168.0.6/repos/Marketing/Crimson_Websites/www.enago.ru@4042 4568d011-afc3-487c-8030-59b888a8bcde

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+$conn = mysql_connect("db143b.pair.com","crimsoni_17","taPiK2dF") or die("could not connect to server");
+
+mysql_set_charset('utf8',$conn); 
+
+mysql_select_db("crimsoni_testimonials",$conn) or die("could not connect to database");
+
+
+?>
