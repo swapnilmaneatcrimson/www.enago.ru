bd295b06b394f037f2fc4bb338a8081483ec9cbf
MKTG_ADMIN <MKTG_ADMIN@4568d011-afc3-487c-8030-59b888a8bcde>
2012-12-18 14:13:31 +0000

    first version
    git-svn-id: http://192.168.0.6/repos/Marketing/Crimson_Websites/www.enago.ru@4062 4568d011-afc3-487c-8030-59b888a8bcde

---
+++ uploadconfig.php
@@ -0,0 +1,17 @@
+<?
+$absolute_path = "/usr/home/crimsonint/public_html/upload/ENAGO_KZ/"; //Absolute path to where files are uploaded
+$absolute_path_translation = "/usr/home/crimsonint/public_html/upload/upload-edisense/translation/"; //Absolute path to where translations files are uploaded
+$absolute_path_invoicesub = "/usr/home/crimsonint/public_html/upload/upload-edisense/invoice/"; //Absolute path to where freelancer invoice files 
+$absolute_pathresume = "/usr/home/crimsonint/public_html/upload/upload-edisense/recruitment_applicant_resume/"; 
+
+$size_limit = "yes"; //do you want a size limit yes or no.
+$limit_size = "12194304"; //How big do you want size limit to be in bytes
+$limit_ext = "no"; //do you want to limit the extensions of files uploaded
+$ext_count = "4"; //total number of extensions in array below
+$extensions = array(".gif", ".jpg", ".jpeg", ".png"); //List extensions you want files uploaded to be
+set_time_limit (0);
+
+$absolute_pathtest = "/usr/www/users/crimsoni/www.edisense.com/test/uploads/"; //Absolute path to where files are uploaded
+$absolute_path_transapp = "/usr/home/crimsonint/public_html/upload/transapp/"; //Absolute path to where files are uploaded
+
+?>
