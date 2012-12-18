1980a038ab75d249f58f4595943beae60a5271dd
MKTG_ADMIN <MKTG_ADMIN@4568d011-afc3-487c-8030-59b888a8bcde>
2012-12-18 14:13:27 +0000

    first version
    git-svn-id: http://192.168.0.6/repos/Marketing/Crimson_Websites/www.enago.ru@4041 4568d011-afc3-487c-8030-59b888a8bcde

---
+++ common.php
@@ -0,0 +1,50 @@
+<?
+//--------------------------- Config file for REQUEST and AUTO and COntact FORM -------------
+//all forms
+$tocs="uploads@enago.com";
+$toec="ec.enago@crimsoni.com, uploads@enago.com";
+
+$toecs="requestecs@enago.com, marketing@enago.com";
+// $autotod = "autoquote@enago.jp"; // autoquote emails
+$tod = "uploads@enago.com, crimsoni2005@gmail.com"; // to the people in the company
+$fromAdd= "uploads@enago.com"; //from which address
+$clientfromAdd= "DO_NOT_REPLY@enago.com"; //client email from address
+$toq = "uploads@enago.com, crimsoni2005@gmail.com";
+// $fromjapanstaff= "japanstaff@enago.jp"; //from which address
+// $torecruitment = "yukof@enago.com, emikot@enago.com";
+
+$tocontact_a = "payments@enago.com"; // if purpose is payment only
+$tocontact_b = "request-ru@enago.com"; // if purpose is other
+
+$tojournal = "journalselection@enago.com"; // journal selection form
+
+$headers = "From: request-ru@enago.com";
+
+$fromAddseform= "DO_NOT_REPLY@enago.com";
+$ec="ec.enago@crimsoni.com";
+$toqc="quality@enago.com";
+
+
+//VCS
+$replyvcs="uploads@enago.com";
+$tovcs="lyazzat.ilimkhanova@gmail.com"; //lyazzats email address for vcs purposes
+//VCS END
+
+//EC Support
+$fromAddecs="requestecs@enago.com";
+
+
+//This is a CC email address. For multiple CC addresses seperate them with "," (comma)
+$bccAdd = "uploads@enago.com"; //this is actually BCC in the code
+$fromAddadvisor="advisor@enago.jp";
+$ratepdf=0;
+$rateexcel=15;
+$ratelatex=0;
+$ratelatextotex=30;
+
+//advance editing
+$advratepdf=0;
+$advrateexcel=15;
+$advratelatex=0;
+$advratelatextotex=30;
+?>
