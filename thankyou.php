62e9a7e537e05d3f8f0932147ed83fcb71bee2be
MKTG_ADMIN <MKTG_ADMIN@4568d011-afc3-487c-8030-59b888a8bcde>
2013-01-07 09:35:51 +0000

    Nikesh: Added Google Adwords Conversion Tracking Code
    git-svn-id: http://192.168.0.6/repos/Marketing/Crimson_Websites/www.enago.ru@6780 4568d011-afc3-487c-8030-59b888a8bcde

--- thankyou.php
+++ thankyou.php
@@ -802,5 +802,24 @@ mail("$email", "$sub", $message1,
 <?php include("inc_footer.htm"); ?>
 <!-- footerContainer -->
 
+<!-- Google Code for Enago Russia Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1011632437;
+var google_conversion_language = "en";
+var google_conversion_format = "2";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "VEEUCJP9_wMQtZKx4gM";
+var google_conversion_value = 0;
+/* ]]> */
+</script>
+<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/1011632437/?value=0&amp;label=VEEUCJP9_wMQtZKx4gM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
 </body>
 </html>
