cf3d7d430e55faf9d1bcadc79e2519b339ac5003
Askok <Askok@4568d011-afc3-487c-8030-59b888a8bcde>
2013-02-13 11:47:20 +0000

    slider of local-editing.htm was not working, corrected
    git-svn-id: http://192.168.0.6/repos/Marketing/Crimson_Websites/www.enago.ru@7859 4568d011-afc3-487c-8030-59b888a8bcde

--- js/jquery-latest.js
+++ js/jquery-latest.js
@@ -7000,7 +7000,7 @@ function css_defaultDisplay( nodeName ) {
 		// document to it; WebKit & Firefox won't allow reusing the iframe document.
 		if ( !iframeDoc || !iframe.createElement ) {
 			iframeDoc = ( iframe.contentWindow || iframe.contentDocument ).document;
-			iframeDoc.write("<!doctype html><html><body>
+			iframeDoc.write("<!doctype html><html><body>");
 			iframeDoc.close();
 		}
 
