8c982f20f8b2e8afd6b82bdd59f9c09c9ea3a5ec
Anand <Anand@4568d011-afc3-487c-8030-59b888a8bcde>
2014-12-22 17:19:53 +0000

    added date function for year display, added new browser code
    git-svn-id: http://192.168.0.6/repos/Marketing/Crimson_Websites/www.enago.ru@22142 4568d011-afc3-487c-8030-59b888a8bcde

--- thankyou.php
+++ thankyou.php
@@ -24,20 +24,105 @@ $nowClock = $nowHour["hours"].":".$nowMin.":".$nowSec;
 $ip=@$REMOTE_ADDR; 
 //****************************************************************************
 
-// important to order by parent ie MSIE then OPERA.
+function getBrowser()
+{
+    $u_agent = $_SERVER['HTTP_USER_AGENT'];
+    $bname = 'Unknown';
+    $platform = 'Unknown';
+    $version= "";
 
-$browser = array (
-   "MSIE",            // parent
-   "OPERA",
-   "MOZILLA",        // parent
-   "NETSCAPE",
-   "FIREFOX",
-   "SAFARI"
-);
+    //First get the platform?
+    if (preg_match('/linux/i', $u_agent)) {
+        $platform = 'linux';
+    }
+    elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+        $platform = 'mac';
+    }
+    elseif (preg_match('/windows|win32/i', $u_agent)) {
+        $platform = 'windows';
+    }
+   
+    // Next get the name of the useragent yes seperately and for good reason
+    if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
+    {
+        $bname = 'Internet Explorer';
+        $ub = "MSIE";
+    }
+	elseif(preg_match('/Trident\/7.0; rv:11.0/',$u_agent))
+    {
+        $bname = 'Internet Explorer';
+    }
+	
+    elseif(preg_match('/Firefox/i',$u_agent))
+    {
+        $bname = 'Mozilla Firefox';
+        $ub = "Firefox";
+    }
+    elseif(preg_match('/Chrome/i',$u_agent))
+    {
+        $bname = 'Google Chrome';
+        $ub = "Chrome";
+    }
+    elseif(preg_match('/Safari/i',$u_agent))
+    {
+        $bname = 'Apple Safari';
+        $ub = "Safari";
+    }
+    elseif(preg_match('/Opera/i',$u_agent))
+    {
+        $bname = 'Opera';
+        $ub = "Opera";
+    }
+    elseif(preg_match('/Netscape/i',$u_agent))
+    {
+        $bname = 'Netscape';
+        $ub = "Netscape";
+    }
+   
+    // finally get the correct version number
+    $known = array('Version', $ub, 'other');
+    $pattern = '#(?<browser>' . join('|', $known) .
+    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+    if (!preg_match_all($pattern, $u_agent, $matches)) {
+        // we have no matching number just continue
+    }
+   
+    // see how many we have
+    $i = count($matches['browser']);
+    if ($i != 1) {
+        //we will have two since we are not using 'other' argument yet
+        //see if version is before or after the name
+        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+            $version= $matches['version'][0];
+        }
+        else {
+            $version= $matches['version'][1];
+        }
+    }
+    else {
+        $version= $matches['version'][0];
+    }
+   
+    // check if we have a number
+    if ($version==null || $version=="") {$version="11";}
+   
+    return array(
+        'userAgent' => $u_agent,
+        'name'      => $bname,
+        'version'   => $version,
+        'platform'  => $platform,
+        'pattern'    => $pattern
+    );
+}
 
-$info[browser] = "OTHER";
+// now try it
+$ua=getBrowser();
+
+$browser = $ua['name'];
+$version = $ua['version'];
+$platform = $ua['userAgent'];
+
 //****************************************************************************
-//alerts
 
 if($clienttype == "New Client")
 {
@@ -240,22 +325,6 @@ $alert1 $alert2 $alert3 $alert4 $alert5 $alert6 $alert7 $alert8 $alert9 $alert10
 
 ///ALERT END
 
-foreach ($browser as $parent) {
-   $s = strpos(strtoupper($_SERVER['HTTP_USER_AGENT']), $parent);
-   $f = $s + strlen($parent);
-   $version = substr($_SERVER['HTTP_USER_AGENT'], $f, 5);
-   $version = preg_replace('/[^0-9,.]/','',$version);
-   
-   if (strpos(strtoupper($_SERVER['HTTP_USER_AGENT']), $parent)) {
-   $info[browser] = $parent;
-   $info[version] = $version;
-   }
-}
-$browser=implode(",", $info);
-
-//****************************************************************************
-
-//
 
 $firstchar = substr($subsubject, 0, 3);
 
