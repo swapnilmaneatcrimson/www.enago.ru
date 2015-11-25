2a64d9f95fc0bbba60bf8fdcda4222f8da6a9ff8
Ashok <Ashok@4568d011-afc3-487c-8030-59b888a8bcde>
2015-11-25 09:34:36 +0000

    Price information removed also removed unwanted folder
    git-svn-id: http://192.168.0.6/repos/Marketing/Crimson_Websites/www.enago.ru@26729 4568d011-afc3-487c-8030-59b888a8bcde

--- enago/js/fontsizer-in.js
+++
@@ -1,9 +0,0 @@
-﻿document.write('<div id=communityThumbsContainer>');  
-document.write('<span class=fonttext>文字サイズ変更 :</span>');
-document.write('<a href=javascript:increaseFontSize();>');
-document.write('<img src=../img/header/font-increase.png class=vabsmiddle width=24 height=17 />');
-document.write('</a>&nbsp;<a href=javascript:decreaseFontSize();>');
-document.write('<img src=../img/header/font-decrease.gif class=vabsmiddle width=24 height=17 />');
-document.write('</a>&nbsp;<a href=javascript:resetFontSize();>');
-document.write('<img src=../img/header/font-normal.png class=vabsmiddle width=24 height=17 /></a>');
-document.write('</div>');
