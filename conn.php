365c0a65b2cd63ad0d893be0b37f0f4368f76c56
Anand <Anand@4568d011-afc3-487c-8030-59b888a8bcde>
2016-01-18 16:26:21 +0000

    AWS to Pair
    git-svn-id: http://192.168.0.6/repos/Marketing/Crimson_Websites/www.enago.ru@27518 4568d011-afc3-487c-8030-59b888a8bcde

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
-$conn = mysql_connect("localhost","user_testimonial","ZdnsYMQdDruVnv3j") or die("could not connect to server");
+$conn = mysql_connect("db143b.pair.com","crimsoni_17","68F09dL3a") or die("could not connect to server");
 
 mysql_set_charset('utf8',$conn); 
 
-mysql_select_db("testimonials_accepted",$conn) or die("could not connect to database");
+mysql_select_db("crimsoni_testimonials",$conn) or die("could not connect to database");
 
 
 ?>
--- uploadconfig.php
+++ uploadconfig.php
@@ -1,5 +1,5 @@
 <?php
-$absolute_path = "/home/upload/non_japan/enago_ru/"; //Absolute path to where files are uploaded
+$absolute_path = "/usr/home/crimsonint/upload/ENAGO_RU/"; //Absolute path to where files are uploaded
 $size_limit = "yes"; //do you want a size limit yes or no.
 $limit_size = "12194304"; //How big do you want size limit to be in bytes
 $limit_ext = "no"; //do you want to limit the extensions of files uploaded
